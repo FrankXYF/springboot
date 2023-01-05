c034836d88c3f915cc5a123d6eaea5a4dec8b0b4
xyf <yefanxu1997@gmail.com>
2023-01-05 11:39:32 +0800

    first commit

---
+++ springboot-helloworld/src/main/java/com/xyf/controller/HelloApplication.java
@@ -0,0 +1,16 @@
+package com.xyf.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author YEFAN
+ * @Description 引导类
+ * @Date 2022/12/27 20:45
+ */
+@SpringBootApplication
+public class HelloApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class,args);
+    }
+}
---
+++ springboot-helloworld/src/main/java/com/xyf/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.xyf.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author YEFAN
+ * @Description
+ * @Date 2022/12/27 20:32
+ */
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public  String hello(){
+        return  "hello Spring Boot !";
+    }
+
+}
